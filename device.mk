d53b953fde87c94414bba65b85c83561286df4d3
Bruno Martins <bgcngm@gmail.com>
2025-03-25 22:12:12 -0300

    peridot: Build opensource soundtrigger HAL
    Change-Id: I956d53a67841a9e1e296de6b73f08ef2efac7470
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- device.mk
+++ device.mk
@@ -60,7 +60,8 @@ PRODUCT_PACKAGES += \
     audioadsprpcd \
     audio.bluetooth.default \
     audio.r_submix.default \
-    audio.usb.default
+    audio.usb.default \
+    sound_trigger.primary.pineapple
 
 PRODUCT_PACKAGES += \
     libagm_compress_plugin \
